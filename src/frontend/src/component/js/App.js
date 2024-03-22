71621dc6d0b4906cb9374e6ff5007a29dbad9d73
yolly98 <gianlucagemini98@gmail.com>
2024-03-22 20:15:48 +0100

    Added frontend skeleton
    Co-authored-by: Biagio Cornacchia <49314349+biagiocornacchia@users.noreply.github.com>
    Co-authored-by: Matteo Abaterusso <49238869+MatteoAba@users.noreply.github.com>

---
+++ src/frontend/src/component/js/App.js
@@ -0,0 +1,14 @@
+import '../css/App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>Frontend</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
